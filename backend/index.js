a8d33d5e91d3ccb117d42cba47c5da7b3c09b128
sharmaharshitnone <sharmaharshitnone@gmail.com>
2025-01-31 11:59:03 +0530

    Done

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 // backend/index.js
 const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 const port = 5000;
+
+app.use(express.json());
+app.use("api/users", userRoutes);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Backend is running");
@@ -10,3 +17,8 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Backend server listening at http://localhost:${port}`);
 });
+
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
